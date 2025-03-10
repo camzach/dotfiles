ae4f1ec8f57f9cb44a8951ae876ce8de7044ce3b
camzach <22503898+camzach@users.noreply.github.com>
2025-03-10 13:13:45 -0400

    nvim:styles for comments/keywords/types

--- dot_config/nvim/lua/plugins/nightfox.lua
+++ dot_config/nvim/lua/plugins/nightfox.lua
@@ -1,6 +1,15 @@
 return {
   "EdenEast/nightfox.nvim",
   config = function()
+    require("nightfox").setup({
+      options = {
+        styles = {
+          comments = "italic",
+          keywords = "bold",
+          types = "italic,bold",
+        },
+      },
+    })
     vim.cmd.colorscheme("carbonfox")
   end,
 }
