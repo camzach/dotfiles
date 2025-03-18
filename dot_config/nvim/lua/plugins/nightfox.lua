171cf8a9a9c381aee462e4b263df9e2a6e660e3b
camzach <22503898+camzach@users.noreply.github.com>
2025-03-18 13:54:13 -0400

    nvim: transparent background

--- dot_config/nvim/lua/plugins/nightfox.lua
+++ dot_config/nvim/lua/plugins/nightfox.lua
@@ -3,6 +3,7 @@ return {
   config = function()
     require("nightfox").setup({
       options = {
+        transparent = "true",
         styles = {
           comments = "italic",
           keywords = "bold",
