705c67357e593b882db820a7f3022cf8e35a49f4
camzach <22503898+camzach@users.noreply.github.com.>
2025-06-03 14:04:13 -0400

    hyprlock: use pipx to run glitch effect

--- dot_config/hypr/hypr-bg/executable_regen-bg.sh
+++ dot_config/hypr/hypr-bg/executable_regen-bg.sh
@@ -1,3 +1,2 @@
-source ~/.config/hypr/hypr-bg/env/bin/activate >/dev/null
-glitch_this -csf -o /tmp/hyprlock/glitched.png /tmp/hyprlock/base.png 7.5 >/dev/null
+pipx run glitch_this -csf -o /tmp/hyprlock/glitched.png /tmp/hyprlock/base.png 7.5 >/dev/null
 echo /tmp/hyprlock/glitched.png
