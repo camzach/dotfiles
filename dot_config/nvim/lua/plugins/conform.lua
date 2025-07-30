cd5254f2a768ae663ca385abaf34c14331d5fe40
camzach <22503898+camzach@users.noreply.github.com>
2025-07-30 13:24:29 -0400

    nvim: use prettier for jsonc

--- dot_config/nvim/lua/plugins/conform.lua
+++ dot_config/nvim/lua/plugins/conform.lua
@@ -1,70 +1,71 @@
 return {
-  "stevearc/conform.nvim",
-  event = "LspAttach",
-  opts = {
-    formatters_by_ft = {
-      typescript = { "prettier" },
-      typescriptreact = { "prettier" },
-      javascript = { "prettier" },
-      javascriptreact = { "prettier" },
-      html = { "prettier" },
-      css = { "prettier" },
-      scss = { "prettier" },
-      markdown = { "prettier" },
-      yaml = { "prettier" },
-      less = { "prettier" },
-      http = { "prettier" },
-      json = { "prettier" },
-      lua = { "stylua" },
-      rust = { "rustfmt" },
-      go = { "gofmt" },
-      zsh = { "shfmt" },
-      sh = { "shfmt" },
-    },
-    formatters = {
-      shfmt = {
-        args = { "-ci", "-bn" },
-      },
-    },
-    format_on_save = function(bufnr)
-      print("Formatting")
-      -- Disable with a global or buffer-local variable
-      if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
-        return
-      end
-      return { timeout_ms = 5000, lsp_format = "fallback" }
-    end,
-  },
-  config = function(_, opts)
-    local conform = require("conform")
-    conform.setup(opts)
-    vim.api.nvim_create_user_command("FormatDisable", function(args)
-      if args.bang then
-        -- FormatDisable! will disable formatting just for this buffer
-        vim.b.disable_autoformat = true
-      else
-        vim.g.disable_autoformat = true
-      end
-    end, {
-      desc = "Disable autoformat-on-save",
-      bang = true,
-    })
-    vim.api.nvim_create_user_command("FormatEnable", function()
-      vim.b.disable_autoformat = false
-      vim.g.disable_autoformat = false
-    end, {
-      desc = "Re-enable autoformat-on-save",
-    })
-    vim.api.nvim_create_user_command("Format", function(args)
-      local range = nil
-      if args.count ~= -1 then
-        local end_line = vim.api.nvim_buf_get_lines(0, args.line2 - 1, args.line2, true)[1]
-        range = {
-          start = { args.line1, 0 },
-          ["end"] = { args.line2, end_line:len() },
-        }
-      end
-      require("conform").format({ async = true, lsp_format = "fallback", range = range })
-    end, { range = true })
-  end,
+	"stevearc/conform.nvim",
+	event = "LspAttach",
+	opts = {
+		formatters_by_ft = {
+			typescript = { "prettier" },
+			typescriptreact = { "prettier" },
+			javascript = { "prettier" },
+			javascriptreact = { "prettier" },
+			html = { "prettier" },
+			css = { "prettier" },
+			scss = { "prettier" },
+			markdown = { "prettier" },
+			yaml = { "prettier" },
+			less = { "prettier" },
+			http = { "prettier" },
+			json = { "prettier" },
+			jsonc = { "prettier" },
+			lua = { "stylua" },
+			rust = { "rustfmt" },
+			go = { "gofmt" },
+			zsh = { "shfmt" },
+			sh = { "shfmt" },
+		},
+		formatters = {
+			shfmt = {
+				args = { "-ci", "-bn" },
+			},
+		},
+		format_on_save = function(bufnr)
+			print("Formatting")
+			-- Disable with a global or buffer-local variable
+			if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+				return
+			end
+			return { timeout_ms = 5000, lsp_format = "fallback" }
+		end,
+	},
+	config = function(_, opts)
+		local conform = require("conform")
+		conform.setup(opts)
+		vim.api.nvim_create_user_command("FormatDisable", function(args)
+			if args.bang then
+				-- FormatDisable! will disable formatting just for this buffer
+				vim.b.disable_autoformat = true
+			else
+				vim.g.disable_autoformat = true
+			end
+		end, {
+			desc = "Disable autoformat-on-save",
+			bang = true,
+		})
+		vim.api.nvim_create_user_command("FormatEnable", function()
+			vim.b.disable_autoformat = false
+			vim.g.disable_autoformat = false
+		end, {
+			desc = "Re-enable autoformat-on-save",
+		})
+		vim.api.nvim_create_user_command("Format", function(args)
+			local range = nil
+			if args.count ~= -1 then
+				local end_line = vim.api.nvim_buf_get_lines(0, args.line2 - 1, args.line2, true)[1]
+				range = {
+					start = { args.line1, 0 },
+					["end"] = { args.line2, end_line:len() },
+				}
+			end
+			require("conform").format({ async = true, lsp_format = "fallback", range = range })
+		end, { range = true })
+	end,
 }
