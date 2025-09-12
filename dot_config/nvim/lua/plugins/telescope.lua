a64b306109b956f03c79d5a30cefbad62e5a3919
camzach <22503898+camzach@users.noreply.github.com>
2025-09-12 15:08:36 -0400

    nvim: telescope grep in dir / grep in open buffers

--- dot_config/nvim/lua/plugins/telescope.lua
+++ dot_config/nvim/lua/plugins/telescope.lua
@@ -1,30 +1,68 @@
+local function live_grep_in_directory()
+	local builtin = require("telescope.builtin")
+
+	builtin.find_files({
+		prompt_title = "Select Directory (press <CR> to grep in)",
+		find_command = { "find", ".", "-type", "d", "-not", "-path", "*/.*" },
+		attach_mappings = function(prompt_bufnr, map)
+			local actions = require("telescope.actions")
+			local action_state = require("telescope.actions.state")
+
+			map("i", "<CR>", function()
+				local selection = action_state.get_selected_entry()
+				actions.close(prompt_bufnr)
+
+				if selection then
+					builtin.live_grep({
+						prompt_title = "Live Grep in " .. selection.value,
+						cwd = selection.value,
+						theme = "ivy",
+					})
+				end
+			end)
+
+			return true
+		end,
+	})
+end
+
+local function live_grep_in_open_buffers()
+	local builtin = require("telescope.builtin")
+	builtin.live_grep({
+		prompt_title = "Live Grep in Open Buffers",
+		grep_open_files = true,
+		theme = "ivy",
+	})
+end
+
 return {
-  "nvim-telescope/telescope.nvim",
-  event = "VeryLazy",
-  opts = {
-    pickers = {
-      grep_string = { previewer = false, theme = "ivy" },
-      diagnostics = { previewer = false, theme = "ivy" },
-      find_files = { theme = "ivy", hidden = true },
-      buffers = { previewer = false, theme = "ivy" },
-      current_buffer_fuzzy_find = { theme = "ivy" },
-      resume = { previewer = false, theme = "ivy" },
-      live_grep = { theme = "ivy" },
-    },
-    defaults = {
-      layout_config = {
-        prompt_position = "bottom",
-      },
-    },
-  },
-  keys = {
-    { "<leader>z", "<cmd>Telescope current_buffer_fuzzy_find<cr>", desc = "File fuzzy find" },
-    { "<leader>d", "<cmd>Telescope diagnostics<cr>", desc = "Show diagnostics" },
-    { "<leader>w", "<cmd>Telescope grep_string<cr>", desc = "Grep string" },
-    { "<leader>f", "<cmd>Telescope find_files<cr>", desc = "Find files" },
-    { "<leader>c", "<cmd>Telescope resume<cr>", desc = "Resume search" },
-    { "<leader>s", "<cmd>Telescope live_grep<cr>", desc = "Live grep" },
-    { "<leader>b", "<cmd>Telescope buffers<cr>", desc = "Buffers" },
-  },
-  dependencies = { "nvim-lua/plenary.nvim" },
+	"nvim-telescope/telescope.nvim",
+	event = "VeryLazy",
+	opts = {
+		pickers = {
+			diagnostics = { previewer = false, theme = "ivy" },
+			find_files = { theme = "ivy", hidden = true },
+			buffers = { previewer = false, theme = "ivy" },
+			current_buffer_fuzzy_find = { theme = "ivy" },
+			resume = { previewer = false, theme = "ivy" },
+			live_grep = { theme = "ivy" },
+		},
+		defaults = {
+			layout_config = {
+				prompt_position = "bottom",
+			},
+		},
+	},
+	keys = {
+		{ "<leader>z", "<cmd>Telescope current_buffer_fuzzy_find<cr>", desc = "File fuzzy find" },
+		{ "<leader>d", "<cmd>Telescope diagnostics<cr>", desc = "Show diagnostics" },
+		{ "<leader>f", "<cmd>Telescope find_files<cr>", desc = "Find files" },
+		{ "<leader>c", "<cmd>Telescope resume<cr>", desc = "Resume search" },
+		{ "<leader>b", "<cmd>Telescope buffers<cr>", desc = "Buffers" },
+		{ "<leader>s", "<cmd>Telescope live_grep<cr>", desc = "Live grep" },
+		{ "<leader>ss", "<cmd>Telescope live_grep<cr>", desc = "Live grep" },
+		{ "<leader>sd", live_grep_in_directory, desc = "Live grep in directory" },
+		{ "<leader>sb", live_grep_in_open_buffers, desc = "Live grep in open buffers" },
+	},
+	dependencies = { "nvim-lua/plenary.nvim" },
 }
