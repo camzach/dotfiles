258531c85043b6fbd942fc1651dd0fba226b87d7
camzach <22503898+camzach@users.noreply.github.com.>
2025-10-09 13:12:59 -0400

    nvim: semantic colors

--- dot_config/nvim/lua/plugins/lualine.lua
+++ dot_config/nvim/lua/plugins/lualine.lua
@@ -2,21 +2,21 @@ local colors = require('config.theme').colors
 
 local theme = {
 	normal = {
-		a = { fg = colors.bg, bg = colors.blue, gui = 'bold' },
+		a = { fg = colors.bg, bg = colors.primary, gui = 'bold' },
 		b = { fg = colors.fg, bg = colors.bg_alt },
 		c = { fg = colors.fg, bg = colors.bg },
 	},
 	insert = {
-		a = { fg = colors.bg, bg = colors.green, gui = 'bold' },
+		a = { fg = colors.bg, bg = colors.success, gui = 'bold' },
 	},
 	visual = {
-		a = { fg = colors.bg, bg = colors.purple, gui = 'bold' },
+		a = { fg = colors.bg, bg = colors.tertiary, gui = 'bold' },
 	},
 	replace = {
-		a = { fg = colors.bg, bg = colors.red, gui = 'bold' },
+		a = { fg = colors.bg, bg = colors.error, gui = 'bold' },
 	},
 	command = {
-		a = { fg = colors.bg, bg = colors.yellow, gui = 'bold' },
+		a = { fg = colors.bg, bg = colors.warning, gui = 'bold' },
 	},
 	inactive = {
 		a = { fg = colors.fg, bg = colors.bg_alt },
