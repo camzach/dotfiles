e72421de982167290dec82d4b370206a13fff9db
camzach <22503898+camzach@users.noreply.github.com.>
2025-10-13 16:45:35 -0400

    nvim: add color previews

---
+++ dot_config/nvim/lua/plugins/highlight-colors.lua
@@ -0,0 +1,18 @@
+return {
+  "brenoprata10/nvim-highlight-colors",
+  opts = {
+    render = 'virtual',
+    virtual_symbol_position = 'eow',
+    virtual_symbol_prefix = ' ',
+    virtual_symbol_suffix = '',
+    enable_hex = true,
+    enable_short_hex = true,
+    enable_rgb = true,
+    enable_hsl = true,
+    enable_ansi = true,
+    enable_hsl_without_function = true,
+    enable_var_usage = true,
+    enable_named_colors = true,
+    enable_tailwind = true,
+  }
+}
