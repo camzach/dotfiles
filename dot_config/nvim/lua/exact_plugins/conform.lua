f26cddee9b910db8d3f3f45afc1455bb2779e5a0
camzach <22503898+camzach@users.noreply.github.com.>
2026-03-24 10:15:55 -0400

    nvim: haskell support

--- dot_config/nvim/lua/exact_plugins/conform.lua
+++ dot_config/nvim/lua/exact_plugins/conform.lua
@@ -21,6 +21,7 @@ return {
       go = { "gofmt" },
       zsh = { "shfmt" },
       sh = { "shfmt" },
+      hs = { "fourmolu" }
     },
     formatters = {
       shfmt = {
--- dot_config/nvim/lua/exact_plugins/lspconfig.lua
+++ dot_config/nvim/lua/exact_plugins/lspconfig.lua
@@ -14,6 +14,7 @@ return {
           "emmet_language_server",
           "eslint",
           "gopls",
+          "hls",
           "jsonls",
           "lua_ls",
           "pyright",
